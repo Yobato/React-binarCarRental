2dec1fe1f7b452bdc59ca8815443130a09880457
Yobato <satriyobagus30077@gmail.com>
2022-04-07 23:11:02 +0700

    menampilkan list data search

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,39 +1,76 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
-    return(
-        <>
-        <footer className="pb-5 pt-2">
-            <div className="container mt-5">
-                <div className="row">
-                    <div className="col-lg-4">
-                        <a className="alamat" href={'/'} style={{paddingRight: '2rem'}}>Jalan Suroyo No. 161 Mayangan Kota
-                            Probolonggo
-                            672000</a><br/><br/>
-                        <a className="alamat" href={'/'}>binarcarrental@gmail.com</a><br/><br/>
-                        <a className="alamat" href={'/'}>081-233-334-808</a><br/><br/>
-                    </div>
-                    <div className="col-lg-2">
-                        <a className="link" href={'/'}>Our services</a><br/><br/>
-                        <a className="link" href={'/'}>Why Us</a><br/><br/>
-                        <a className="link" href={'/'}>Testimonial</a><br/><br/>
-                        <a className="link" href={'/'}>FAQ</a><br/><br/>
-                    </div>
-                    <div className="col-lg-4">
-                        <p>Connect with us</p>
-                        <img src="asset\img\list_item.png" style={{maxWidth: '224px', cursor: 'pointer'}} alt='item'/><br/><br/>
-                    </div>
-                    <div className="col-lg-2">
-                        <p href='/'>Copyright Binar 2022</p>
-                        <button type="button" className="btn btn-nav me-0" style={{backgroundColor: '#0D28A6'}}>
-                            <span style={{visibility: 'hidden'}}>Copyrighted</span>
-                        </button>
-                    </div>
-                </div>
+  return (
+    <>
+      <footer className="pb-5 pt-2">
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-lg-4">
+              <a className="alamat " href="#" style={{ paddingRight: "2rem" }}>
+                Jalan Suroyo No. 161 Mayangan Kota Probolonggo 672000
+              </a>
+              <br />
+              <br />
+              <a className="alamat" href="#">
+                binarcarrental@gmail.com
+              </a>
+              <br />
+              <br />
+              <a className="alamat" href="#">
+                081-233-334-808
+              </a>
+              <br />
+              <br />
+            </div>
+            <div className="col-lg-2">
+              <a className="link" href="#">
+                Our services
+              </a>
+              <br />
+              <br />
+              <a className="link" href="#">
+                Why Us
+              </a>
+              <br />
+              <br />
+              <a className="link" href="#">
+                Testimonial
+              </a>
+              <br />
+              <br />
+              <a className="link" href="#">
+                FAQ
+              </a>
+              <br />
+              <br />
+            </div>
+            <div className="col-lg-4">
+              <p>Connect with us</p>
+              <img
+                src="./img/list_item.png"
+                style={{ maxWidth: "224px", cursor: "pointer" }}
+                alt="item"
+              />
+              <br />
+              <br />
+            </div>
+            <div className="col-lg-2">
+              <p href="/">Copyright Binar 2022</p>
+              <button
+                type="button"
+                className="btn btn-nav me-0"
+                style={{ backgroundColor: "#0D28A6" }}
+              >
+                <span style={{ visibility: "hidden" }}>Copyrighted</span>
+              </button>
+            </div>
+          </div>
         </div>
-        </footer>
-        </>
-    )
+      </footer>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import SearchHome from "../components/SearchHome";  
+import "./Home.css"
 
 function Home() {
   return (
-      <>
-        <Header/>
-        <div style={{ marginTop : '5%' }}>Halo ini Halaman Utama mu</div>
-        <Footer/>
-      </>
-  )
+    <>
+      <Header />
+      <div id="halaman-1" className="halaman-1">
+        <div className="container-fluid pt-5">
+          <div className="row d-flex align-items-center mt-5">
+            <div className="col-lg-6">
+              <div className="row justify-content-center">
+                <div className="col-lg-8">
+                  <h1
+                    className="judul"
+                    style={{
+                      fontSize: "36px",
+                      lineHeight: "54px",
+                      maxWidth: "568px",
+                    }}
+                  >
+                    Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)
+                  </h1>
+                  <p style={{ fontSize: "14px" }}>
+                    Selamat datang di Binar Car Rental. Kami menyediakan mobil
+                    kualitas terbaik dengan harga terjangkau. Selalu siap
+                    melayani kebutuhanmu untuk sewa mobil selama 24 jam.
+                  </p>
+                  {/* <button className="btn btn-utama mb-4 fw-bold">
+                    Mulai Sewa Mobil
+                  </button> */}
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 p-0 m-0">
+              <img src="./img/img_car.png" className="w-100" alt="Mobil Home" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <SearchHome />
+      <Footer />
+    </>
+  );
 }
 
-export default Home
+export default Home;
